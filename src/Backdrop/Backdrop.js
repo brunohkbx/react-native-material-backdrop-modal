246406a8f3a93b714a4a8f7db3a84d87f10355e1
Bruno <bruno.castro@codeminer42.com>
2019-10-01 00:24:58 -0300

    feat: add component Backdrop

---
+++ src/Backdrop/Backdrop.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+import {
+  LayoutAnimation,
+  Platform,
+  StyleSheet,
+  UIManager,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import useUpdateEffect from 'react-use/lib/useUpdateEffect';
+import Subheader from './Subheader';
+
+const Backdrop = ({ children, focused, onFocus, title, icon }) => {
+  const [contentVisibility, setContentVisibility] = useState({
+    alignItems: 'stretch',
+    displayChildren: true,
+    concealed: false,
+  });
+
+  useEffect(() => {
+    if (
+      Platform.OS === 'android' &&
+      UIManager.setLayoutAnimationEnabledExperimental
+    ) {
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  }, []);
+
+  useUpdateEffect(() => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
+    if (focused) {
+      setContentVisibility({
+        alignItems: 'stretch',
+        displayChildren: true,
+        concealed: false,
+      });
+    } else {
+      setContentVisibility({
+        alignItems: 'flex-end',
+        displayChildren: false,
+        concealed: true,
+      });
+    }
+  }, [focused]);
+
+  return (
+    <View style={[styles.modal, { alignItems: contentVisibility.alignItems }]}>
+      <View style={styles.backdrop}>
+        <Subheader
+          disabled={focused}
+          onPress={onFocus}
+          icon={icon}
+          showIcon={contentVisibility.concealed}
+          title={title}
+        />
+        {children && contentVisibility.displayChildren && (
+          <View style={styles.contentContainer}>{children}</View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+Backdrop.propTypes = {
+  children: PropTypes.node,
+  focused: PropTypes.bool,
+  onFocus: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.element,
+};
+
+Backdrop.defaultProps = {
+  children: null,
+  focused: true,
+  icon: null,
+};
+
+const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    flexDirection: 'row',
+    ...StyleSheet.absoluteFill,
+  },
+  backdrop: {
+    backgroundColor: '#FFF',
+    flex: 1,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+  },
+  contentContainer: {
+    marginHorizontal: 16,
+    paddingVertical: 12,
+  },
+});
+
+export default Backdrop;
---
+++ src/Backdrop/Subheader.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Touchable from 'react-native-platform-touchable';
+import PropTypes from 'prop-types';
+
+const Subheader = ({ disabled, onPress, icon, showIcon, title }) => {
+  return (
+    <Touchable
+      disabled={disabled}
+      onPress={onPress}
+      background={Touchable.Ripple('rgba(0, 0, 0, 0.2)', true)}
+    >
+      <View style={styles.headerContainer}>
+        <Text style={styles.title}>{title}</Text>
+        {icon && showIcon && (
+          <Touchable
+            onPress={onPress}
+            centered
+            style={styles.icon}
+            background={Touchable.Ripple('rgba(0, 0, 0, 0.2)', true)}
+          >
+            {icon}
+          </Touchable>
+        )}
+      </View>
+    </Touchable>
+  );
+};
+
+Subheader.propTypes = {
+  disabled: PropTypes.bool,
+  onPress: PropTypes.func.isRequired,
+  icon: PropTypes.element,
+  showIcon: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+};
+
+Subheader.defaultProps = {
+  disabled: false,
+  icon: null,
+  showIcon: false,
+};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'rgba(0, 0, 0, 0.12)',
+    marginHorizontal: 16,
+    paddingVertical: 12,
+  },
+  title: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: 'rgba(0, 0, 0, 0.87)',
+    fontFamily: 'sans-serif',
+  },
+  icon: {
+    position: 'absolute',
+    top: 12,
+    right: 16,
+    width: 24,
+    height: 24,
+  },
+});
+
+export default Subheader;
