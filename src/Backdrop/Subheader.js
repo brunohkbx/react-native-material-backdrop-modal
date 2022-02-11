196f1c020e9eef9ad004899f12faf31096ca2524
Enieber Cunha <7907068+enieber@users.noreply.github.com>
2022-02-10 21:51:19 -0300

    fix: eslint erro with  prettier plugin

--- src/Backdrop/Subheader.js
+++ src/Backdrop/Subheader.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Touchable from 'react-native-platform-touchable';
-import PropTypes from 'prop-types';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Touchable from "react-native-platform-touchable";
+import PropTypes from "prop-types";
 
-const Subheader = ({ disabled, onPress, icon, title, titleStyle, TitleComponent }) => {
+const Subheader = ({
+  disabled,
+  onPress,
+  icon,
+  title,
+  titleStyle,
+  TitleComponent,
+}) => {
   return (
     <Touchable
       disabled={disabled}
       onPress={onPress}
-      background={Touchable.Ripple('rgba(0, 0, 0, 0.2)', true)}
+      background={Touchable.Ripple("rgba(0, 0, 0, 0.2)", true)}
       accessibilityState={{ expanded: disabled }}
       accessibilityLabel={title}
       accessibilityRole="button"
     >
       <View style={styles.headerContainer}>
-        {TitleComponent ? 
-          (<TitleComponent />) : 
-          (<Text style={[styles.title, titleStyle]}>{title}</Text>)
-        }
+        {TitleComponent ? (
+          <TitleComponent />
+        ) : (
+          <Text style={[styles.title, titleStyle]}>{title}</Text>
+        )}
         {icon && (
           <Touchable
             onPress={onPress}
             centered
             style={styles.icon}
-            background={Touchable.Ripple('rgba(0, 0, 0, 0.2)', true)}
+            background={Touchable.Ripple("rgba(0, 0, 0, 0.2)", true)}
             testID="icon"
             hitSlop={{ top: 20, left: 20, bottom: 20, right: 20 }}
             accessibilityRole="button"
@@ -54,17 +62,17 @@ Subheader.defaultProps = {
 const styles = StyleSheet.create({
   headerContainer: {
     borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: 'rgba(0, 0, 0, 0.12)',
+    borderBottomColor: "rgba(0, 0, 0, 0.12)",
     marginHorizontal: 16,
     paddingVertical: 12,
   },
   title: {
     fontSize: 16,
     lineHeight: 24,
-    color: 'rgba(0, 0, 0, 0.87)',
+    color: "rgba(0, 0, 0, 0.87)",
   },
   icon: {
-    position: 'absolute',
+    position: "absolute",
     top: 12,
     right: 16,
     width: 24,
